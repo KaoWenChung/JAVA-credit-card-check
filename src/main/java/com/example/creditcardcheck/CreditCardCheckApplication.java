6178cca1b25da2003146a372a73dcadd4f4eeae0
wyn <k022298@gmail.com>
2024-03-10 13:15:03 +0000

    Initialized project

---
+++ src/main/java/com/example/creditcardcheck/CreditCardCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditcardcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/creditcardcheck/CreditCardCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creditcardcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
